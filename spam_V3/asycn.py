ce6b8e3e95b3c3524bf9ee4d16daf6a97cf72536
Kadawatcha <163724568+Kadawatcha@users.noreply.github.com>
2026-08-18 17:23:49 +0200

    Ajout de la gestion des statuts de réponse et amélioration de l'affichage dans la fonction main

--- spam_V3/asycn.py
+++ spam_V3/asycn.py
@@ -3,10 +3,14 @@ Pour les commentaires spécifiques a des variables "de base" voir /spam_V3/sync.
 '''
  
 import httpx     
+import asyncio
+import sys # pour faire joli dans le terminal
 from requests import Response # biblio synchrone oui mais utilisée uniquement pour le type 
 
 
-async def send_request():
+CHARGEMENT = ["⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏"]
+
+async def send_request() -> int: # renvoie un entier
     url = "http://127.0.0.1:8000/login"
     donnees = {                                                                                  
         "username": "fake_user",                                                    
@@ -19,18 +23,47 @@ async def send_request():
     
     async with httpx.AsyncClient() as client:
         reponse: Response = await client.post(url, datas=donnees) #hearders=headers
-        
         # Afficher le dictionnaire complet des en-têtes renvoyés par le serveur                                                 
-        # print("En-têtes reçus :", reponse.headers)   
-        
+        # print("En-têtes reçus :", reponse.headers)  
         statut = reponse.status_code                                                
-        if statut == 200:                                                                            
-            print("✅ Succès ! Mot de passe et user correct") 
-        elif statut == 404:                                                                          
-            print("❌ Erreur 404 : La page n'a pas été trouvée.")                                    
-        elif statut == 401 or statut == 403:                                                         
-            print("🔒 Accès refusé (Non autorisé).")                                                 
-        else:                                                                                        
-            print(f"⚠️ Autre code reçu : {statut}")   
+        return statut
                                                                          
-                                                                                                 
+async def main():
+    print('🚀 Démarrage du script...')
+    while True:
+        try:
+            
+            symbole = CHARGEMENT[compteur_animation % len(CHARGEMENT)]
+            tentatives += 1
+        
+        #    \r remet le curseur au début de la ligne, end="" évite le saut de ligne
+            sys.stdout.write(f"\r{symbole} Recherche en cours... Tentative n°{tentatives}")
+            sys.stdout.flush()
+            compteur_animation += 1
+            
+            statut = await send_request()
+            await asyncio.sleep(0.1) # eviter de faire planter le pc de la ménagère 
+            if statut == 200:      
+                sys.stdout.write("\r" + " " * 50 + "\r")                                                                      
+                print("✅ Succès ! Mot de passe et user correct") 
+                break
+            elif statut == 404: 
+                print('404')                                                                         
+                break                  
+            elif statut == 401:                                                   
+                pass           
+            elif statut == 403:
+                print('403 - Interdit')
+                break                                      
+            else:                                                                                        
+                sys.stdout.write("\r" + " " * 50 + "\r")
+                print(f"⚠️ Autre code reçu : {statut}")   
+                await asyncio.sleep(2) # Pause plus longue en cas d'anomalie
+                          
+                        
+        except Exception as e:
+            print(f"Erreur réseau détectée : {e}")
+            # Pause plus longue en cas d'erreur
+            await asyncio.sleep(10)
+
+asyncio.run(main()) 

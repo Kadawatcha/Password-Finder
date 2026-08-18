49e06f17559c3a45710f98c9d911a53774ba64f3
Kadawatcha <163724568+Kadawatcha@users.noreply.github.com>
2026-08-18 16:56:06 +0200

    async de v3

---
+++ spam_V3/asycn.py
@@ -0,0 +1,27 @@
+'''VERSION ASYNCHRONE : ENVOI REQUETES EN MASSE ET ATTENDS LA REPONSE
+Pour les commentaires spécifiques a des variables "de base" voir /spam_V3/sync.py
+'''
+ 
+import httpx     
+from requests import Response # biblio synchrone oui mais utilisée uniquement pour le type 
+
+
+async def send_request():
+    url = "http://127.0.0.1:8000/login"
+    donnees = {                                                                                  
+        "username": "fake_user",                                                    
+        "password": "123456"                                                           
+    }
+    headers = {                                                                                                             
+            "User-Agent": "Mon-Script-Asynchrone-V2",                                                                               
+            "Accept": "text/html,application/xhtml+xml"                                                                             
+        }     
+    
+    async with httpx.AsyncClient() as client:
+        reponse: Response = await client.post(url, datas=donnees) #hearders=headers
+        print(f"Statut : {reponse.status_code}")  
+        
+        # Afficher le dictionnaire complet des en-têtes renvoyés par le serveur                                                 
+        # print("En-têtes reçus :", reponse.headers)                   
+                                                                         
+                                                                                                 
